7a0b1ab82c9215c48bae866322da9629d649578f
*Unais <mnrunais@gmail.com>
2024-03-26 09:15:07 +0530

    appbar and tabbar desing added

---
+++ lib/view/home/widgets/all_songs.dart
@@ -0,0 +1,27 @@
+// import 'package:flutter/material.dart';
+
+// class AllSongs extends StatelessWidget {
+//   const AllSongs({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.separated(
+//       itemBuilder: (context, index) => ListTile(),
+//       separatorBuilder: (context, index) => SizedBox(),
+//       itemCount: 20,
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+
+class AllSongs extends StatelessWidget {
+  const AllSongs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('AllSongs'),
+    );
+  }
+}
