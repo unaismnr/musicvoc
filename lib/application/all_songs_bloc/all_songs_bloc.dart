0c84cf4d71644bf322e442c3af7f6a6b8aeee514
Billme Cart <mnrunais@gmail.com>
2024-06-09 21:05:30 +0530

    permission issue fixed

--- lib/application/all_songs_bloc/all_songs_bloc.dart
+++ lib/application/all_songs_bloc/all_songs_bloc.dart
@@ -1,3 +1,41 @@
+// import 'dart:developer';
+
+// import 'package:bloc/bloc.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
+// import 'package:musicvoc/services/audio_service/get_audios_repo.dart';
+// import 'package:on_audio_query/on_audio_query.dart';
+// import 'package:permission_handler/permission_handler.dart';
+
+// part 'all_songs_event.dart';
+// part 'all_songs_state.dart';
+// part 'all_songs_bloc.freezed.dart';
+
+// class AllSongsBloc extends Bloc<AllSongsEvent, AllSongsState> {
+//   AllSongsBloc() : super(AllSongsState.initial()) {
+//     on<_FetchSongs>((event, emit) async {
+//       final status = await Permission.storage.request();
+//       if (status.isGranted) {
+//         try {
+//           final songs = await GetAudiosRepo.getAudios();
+//           final filteredSongs = songs
+//               .where((song) =>
+//                   song.fileExtension == 'mp3' || song.fileExtension == 'm4a')
+//               .toList();
+//           for (var item in songs) {
+//             if (item.fileExtension == 'mp3') {
+//               emit(state.copyWith(allSongs: filteredSongs));
+//             }
+//           }
+//         } catch (e) {
+//           log("All Songs Fetching Error: ${e.toString()}");
+//         }
+//       } else if (status.isDenied) {
+//         log('Permission Denied');
+//       }
+//     });
+//   }
+// }
+
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
@@ -13,25 +51,32 @@ part 'all_songs_bloc.freezed.dart';
 class AllSongsBloc extends Bloc<AllSongsEvent, AllSongsState> {
   AllSongsBloc() : super(AllSongsState.initial()) {
     on<_FetchSongs>((event, emit) async {
-      final status = await Permission.storage.request();
-      if (status.isGranted) {
+      if (await _requestPermissions()) {
         try {
           final songs = await GetAudiosRepo.getAudios();
           final filteredSongs = songs
-              .where((song) =>
-                  song.fileExtension == 'mp3' || song.fileExtension == 'm4a')
+              .where(
+                (song) =>
+                    song.fileExtension == 'mp3' || song.fileExtension == 'm4a',
+              )
               .toList();
-          for (var item in songs) {
-            if (item.fileExtension == 'mp3') {
-              emit(state.copyWith(allSongs: filteredSongs));
-            }
-          }
+          emit(state.copyWith(allSongs: filteredSongs));
         } catch (e) {
           log("All Songs Fetching Error: ${e.toString()}");
         }
-      } else if (status.isDenied) {
+      } else {
         log('Permission Denied');
       }
     });
+  }
+
+  Future<bool> _requestPermissions() async {
+    if (await Permission.audio.request().isGranted) {
+      return true;
+    } else if (await Permission.storage.request().isGranted) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
